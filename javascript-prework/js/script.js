1c97b06d4a15503638d9c350a6d7abf3df0a8444
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-22 05:26:15 +0100

    last

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -34,6 +34,17 @@ function displayResult(argComputerMove, argPlayerMove) {
   }
 }
 
+function playGame(playerInput) {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+
+  printMessage('Mój ruch to: ' + computerMove);
+  let playerMove = getMoveName(parseInt(playerInput));
+  printMessage('Twój ruch to: ' + playerMove);
+
+  displayResult(computerMove, playerMove);
+}
+
 document.getElementById('play-rock').addEventListener('click', function () {
   playGame(1);
 });
@@ -43,21 +54,3 @@ document.getElementById('play-paper').addEventListener('click', function () {
 document.getElementById('play-scissors').addEventListener('click', function () {
   playGame(3);
 });
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(randomNumber);
-
-printMessage('Mój ruch to: ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = getMoveName(parseInt(playerInput));
-
-printMessage('Twój ruch to: ' + playerMove);
-
-displayResult(computerMove, playerMove);
