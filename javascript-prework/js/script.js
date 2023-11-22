edd9c9218d942703b7d807dc83c79ed0b5635c8e
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-22 02:29:13 +0100

    kamienpapier

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -2,8 +2,11 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+    let computerMove = `kamień`;
+    let playerMove = `papier`;
+        printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+        printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 }
-
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
