edf1e278af71147d4f5e53bc766d4b8baa485568
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-22 03:31:03 +0100

    3xwybor

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,12 +1,17 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-    let computerMove = `kamień`;
-    let playerMove = `papier`;
-        printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-        printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+else if (randomNumber === 2) {
+    computerMove = 'papier';
+} 
+else if (randomNumber === 3) {
+    computerMove = 'nożyce';
 }
+
+printMessage('Mój ruch to: ' + computerMove);
