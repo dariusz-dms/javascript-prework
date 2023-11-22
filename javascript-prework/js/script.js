8ffed5562fb5f6215f42bacda4fd853efe975b6c
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-22 05:05:59 +0100

    addscriptfinal

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -34,6 +34,16 @@ function displayResult(argComputerMove, argPlayerMove) {
   }
 }
 
+document.getElementById('play-rock').addEventListener('click', function () {
+  playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function () {
+  playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function () {
+  playGame(3);
+});
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
