9ecf5522a5816534130f3f5c3ee4931bc7467612
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-24 04:47:15 +0100

    olderjv' git commit -m olderjv
    exti
    
    git tree

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-function removePreviousResult() {
-    let gameResults = document.getElementById('game-results');
-    let results = gameResults.children;
-    
-    if (results.length >= 2) {
-      let previousResult = results[results.length - 2];
-      gameResults.removeChild(previousResult);
-    }
-  }
