843881bf71722169dadc91472b2d5c61a8caf0fe
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-11-24 04:25:40 +0100

    fixnewjv

--- js/script.js
+++ js/script.js
@@ -22,20 +22,27 @@ function displayResults() {
   }
 }
 
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3 + 1);
+}
+
 function displayResult(argComputerMove, argPlayerMove) {
-  let resultMessage = 'Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove;
+  let playerMoveName = getMoveName(parseInt(argPlayerMove));
+  let computerMoveName = getMoveName(getRandomNumber());
+
+  let resultMessage = 'Zagrałeś ' + playerMoveName + ', komputer zagrał ' + computerMoveName + '. ';
 
   if (argPlayerMove === 'nieznany ruch') {
-    resultMessage = 'Wprowadź poprawny ruch!';
+    resultMessage += 'Wprowadź poprawny ruch!';
   } else if (argComputerMove === argPlayerMove) {
-    resultMessage = 'Remis!';
+    resultMessage += 'Remis!';
   } else if (
     (argComputerMove === 'kamień' && argPlayerMove === 'papier') ||
     (argComputerMove === 'papier' && argPlayerMove === 'nożyce') ||
     (argComputerMove === 'nożyce' && argPlayerMove === 'kamień')
   ) {
     consecutiveWins++;
-    resultMessage = 'Ty wygrywasz!';
+    resultMessage += 'Wygrałeś!';
 
     if (consecutiveWins === 2) {
       playFanfare('https://drive.google.com/uc?id=1G9Y1plAw_x3hC0ialxYqBtMLBmqo1EEW');
@@ -43,7 +50,7 @@ function displayResult(argComputerMove, argPlayerMove) {
     }
   } else {
     consecutiveWins = 0;
-    resultMessage = 'Komputer wygrywa!';
+    resultMessage += 'Komputer wygrał!';
   }
 
   gameResults.push(resultMessage);
