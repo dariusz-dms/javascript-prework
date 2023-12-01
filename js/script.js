f4063554ae701013b90fb60b3cf2bd7c0bc63b05
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-12-01 01:06:22 +0100

    add_result

--- js/script.js
+++ js/script.js
@@ -1,74 +1,78 @@
 {
   const maxResults = 3;
-  
+
   {
     const gameResults = [];
     const movesHistory = [];
 
-    const createDiv = msg => {
-      const div = document.createElement('div');
-      div.innerHTML = msg;
-      return div;
-    };
-
     const printMessage = (msg, containerId) => {
-      const div = createDiv(msg);
-      document.getElementById(containerId).appendChild(div);
+      const createDiv = () => {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        document.getElementById(containerId).appendChild(div);
+      };
+      createDiv();
     };
 
-    const getRecentResults = () => {
+    const printResults = () => {
       const startIndex = Math.max(0, gameResults.length - maxResults);
-      return gameResults.slice(startIndex);
-    };
-
-    const displayGameResults = () => {
+      const recentResults = gameResults.slice(startIndex);
+      
       document.getElementById('game-results').innerHTML = '';
-      for (const result of getRecentResults()) {
+      
+      for (const result of recentResults) {
         printMessage(result, 'game-results');
       }
     };
 
-    const getRecentMoves = () => {
-      const startIndex = Math.max(0, movesHistory.length - maxResults);
-      return movesHistory.slice(startIndex);
+    const printMovesHistory = () => {
+      const getRecentMoves = () => {
+        const startIndex = Math.max(0, movesHistory.length - maxResults);
+        return movesHistory.slice(startIndex);
+      };
+
+      const recentMoves = getRecentMoves();
+
+      const displayMovesHistory = () => {
+        document.getElementById('moves-history').innerHTML = '';
+        for (const move of recentMoves) {
+          printMessage(move, 'moves-history');
+        }
+      };
+      displayMovesHistory();
     };
 
-    const displayMovesHistory = () => {
-      document.getElementById('moves-history').innerHTML = '';
-      for (const move of getRecentMoves()) {
-        printMessage(move, 'moves-history');
-      }
-    };
-
-    const getMoveName = argMoveId => {
-      switch (argMoveId) {
-        case 1:
-          return 'kamień';
-        case 2:
-          return 'papier';
-        case 3:
-          return 'nożyce';
-        default:
-          return 'nieznany ruch';
-      }
+    const getMoveName = (argMoveId) => {
+      const getMoveType = () => {
+        switch (argMoveId) {
+          case 1:
+            return 'kamień';
+          case 2:
+            return 'papier';
+          case 3:
+            return 'nożyce';
+          default:
+            return 'nieznany ruch';
+        }
+      };
+      return getMoveType();
     };
 
     let consecutiveWins = 0;
 
-    const playSound = soundUrl => {
-      const sound = new Audio(soundUrl);
-      sound.volume = 0.5;
-      sound.play();
-    };
-
-    const playFanfare = soundUrl => {
-      playSound(soundUrl);
+    const playFanfare = (soundUrl) => {
+      const playSound = () => {
+        const sound = new Audio(soundUrl);
+        sound.volume = 0.5;
+        sound.play();
+      };
+      playSound();
     };
 
     const displayResult = (argComputerMove, argPlayerMove) => {
       const moveMsg = `Mój ruch to: ${argComputerMove}, Twój ruch to: ${argPlayerMove}`;
       movesHistory.push(moveMsg);
-      displayMovesHistory();
+      printMovesHistory();
 
       if (argPlayerMove === 'nieznany ruch') {
         gameResults.push('Wprowadź poprawny ruch!');
@@ -95,10 +99,10 @@
         gameResults.shift();
       }
 
-      displayResults();
+      printResults();
     };
 
-    const playGame = playerInput => {
+    const playGame = (playerInput) => {
       console.log('Wywołano funkcję playGame. Gracz wybrał: ' + playerInput);
       const randomNumber = Math.floor(Math.random() * 3 + 1);
       const computerMove = getMoveName(randomNumber);
@@ -114,10 +118,12 @@
       console.log('Kliknięto guzik Kamień');
       playGame(1);
     });
+
     document.getElementById('play-paper').addEventListener('click', () => {
       console.log('Kliknięto guzik Papier');
       playGame(2);
     });
+
     document.getElementById('play-scissors').addEventListener('click', () => {
       console.log('Kliknięto guzik Nożyce');
       playGame(3);
